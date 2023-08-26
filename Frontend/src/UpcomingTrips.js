8b80976c6b1817cf21708558e0fbbb0d6244ec5a
Ujjwal Sinha <sinhaujjwal04@gmail.com>
2023-08-26 09:36:53 +0530

    added

--- Frontend/src/UpcomingTrips.js
+++ Frontend/src/UpcomingTrips.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './UpcomingTrips.css'; // Import your custom CSS file for additional styling
 
 function UpcomingTrips() {
   return (
-    <div>
-    <button className='btn btn-success w-100 rounded-0'> View Upcoming Flight Trips</button>
-    <p></p>
-    <button>View Upcoming Train Trips</button>
-    <p></p>
-    <button>View Upcoming Bus Trips</button>
+    <div className="upcoming-trips">
+      <div className="trip-card">
+        <button className="btn btn-card btn-success w-100 rounded-0">
+          View Upcoming Flight Trips
+        </button>
+        <div className="card-hover-image flight"></div>
+      </div>
+
+      <div className="trip-card">
+        <button className="btn btn-card btn-success w-100 rounded-0">
+          View Upcoming Train Trips
+        </button>
+        <div className="card-hover-image train"></div>
+      </div>
+
+      <div className="trip-card">
+        <button className="btn btn-card btn-success w-100 rounded-0">
+          View Upcoming Bus Trips
+        </button>
+        <div className="card-hover-image bus"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UpcomingTrips
+export default UpcomingTrips;
