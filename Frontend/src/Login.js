ee4a1a14d53344a853591ac46fbcfbb23016230e
Ujjwal Sinha <sinhaujjwal04@gmail.com>
2023-08-27 05:22:34 +0530

    new

--- Frontend/src/Login.js
+++ Frontend/src/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import validation from './LoginValidation'
+import './Login.css'
 function Login() 
 {
   const[values,setValues]=useState({email:'',
@@ -15,34 +16,32 @@ function Login()
   }
  
   return (
-    <div className='d-flex justify-content-center align-items-center bg-black vh-100'>
-        <div className='bg-white p-3 rounded w-25'>
-        <h2>Sign-In</h2>
-        <form action='' onSubmit={submitHandler}>
-        <div className='mb-3'>
-           <label htmlFor='email'><strong>Email</strong></label>
-              <input type="text" placeholder='Enter Email' name='email'
-              onChange={handleChange} className='form-control rounded-0'/>
-              {errors.email && <span className='text-danger'>{errors.email}</span>}
-         </div>
-        <div className='mb-3'>
-             <label htmlFor='password'><strong>Password</strong></label>
-             <input type="password" placeholder='Enter Password' name='password' 
-               onChange={handleChange} className='form-control rounded-0'/>
-               {errors.password && <span className='text-danger'>{errors.password}</span>}
-             </div>
 
-            <div className='mb-3'>
-              <p></p>
-            <Link to="/home" type="submit " className='btn btn-success w-100 rounded-0'>Log In</Link>
-            <p> You agree  to our terms </p>
-            <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>NewUser?SignUp</Link>
-
-            </div>
-           
-        </form>
-        </div>
-    </div>
+        <div class="login-box">
+               <h2>Sign-In</h2>
+                <form action='' onSubmit={submitHandler}> 
+                   <div class="user-box">
+                        <input type="email"  required=""   name='email' onChange={handleChange}/>
+                        {errors.email && <span className='text-danger'>{errors.email}</span>}
+                        <label htmlFor='email'>Email</label>
+                  </div>
+                  <div class="user-box">
+                        <input type="password"  required=""   name='password' onChange={handleChange}/>
+                        {errors.password && <span className='text-danger'>{errors.password}</span>}
+                        <label htmlFor='password'>Password</label>
+                  </div>
+    
+    <Link to="/home" href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Sign-In
+    </Link>
+  </form>
+</div>
+  
+    
   )
 }
 
